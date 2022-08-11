71b0a0d0dc962679b7268ce47b5cfd2894441b11
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-11 15:04:34 -0300

    :heavy_plus_sign: feat: add date-fns and format current date

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
 import styles from './styles.module.scss';
 
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
 export function Header() {
-  const currentDate = new Date().toLocaleDateString();
+  const currentDate = format(new Date(), 'EEEEEE, d MMM', {
+    locale: ptBR,
+  });
 
   return (
     <header className={styles.headerContainer}>
