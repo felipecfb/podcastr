9129d515e78626d7a3ba5581f854f92dfdfbf57d
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-12 10:15:04 -0300

    :heavy_plus_sign: add axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,49 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from "next";
 
-export default function Home() {
-  return (
-    <h1>Hello!</h1>
-  )
+type Episode = {
+  episodes: Array<{
+    id: string;
+    title: string;
+    members: string;
+    published_at: string;
+    thumbnail: string;
+    descriptipn: string;
+    file: {
+      url: string;
+      type: string;
+      duration: string;
+    };
+  }>;
+};
+
+interface Episodes {
+  episodes: Episode[];
 }
+
+export default function Home(episodes: Episodes) {
+  console.log(episodes);
+
+  return <div>{JSON.stringify(episodes)}</div>;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch(
+    "http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc"
+  );
+  const data = await response.json();
+
+  const episodes = data.map((episode: Episode) => {
+    return {
+      ...episode,
+    };
+  });
+
+  console.log(episodes);
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
