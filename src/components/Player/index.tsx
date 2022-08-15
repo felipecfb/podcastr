65f6710e9bc1d4ce16bd031eda966edff1638ac8
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-15 10:00:56 -0300

    :sparkles: feat: Creating current episode on player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useContext } from "react";
 import { PlayerContext } from "../../contexts/PlayerContext";
 import styles from "./styles.module.scss";
@@ -14,9 +15,22 @@ export function Player() {
         <strong>Tocando agora {episode?.title}</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
 
       <footer className={styles.empty}>
         <div className={styles.progress}>
