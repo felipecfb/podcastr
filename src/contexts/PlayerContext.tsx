66167db115d03c0f99e3c875f42185274e01c28e
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-15 12:16:41 -0300

    :sparkles: feat: Creating shuffling control

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -17,10 +17,12 @@ type PlayerContextData = {
   currentEpisodeIndex: number;
   isPlaying: boolean;
   isLooping: boolean;
+  isShuffling: boolean;
   play: (episode: Episode) => void;
   playList: (episodeList: Episode[], index: number) => void;
   tooglePlay: () => void;
   toogleLoop: () => void;
+  toggleShuffle: () => void;
   setPlayingState: (state: boolean) => void;
   playNext: () => void;
   playPrevious: () => void;
@@ -37,6 +39,7 @@ export function PlayerContextProvider({
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   function play(episode: Episode) {
     setEpisodeList([episode]);
@@ -58,17 +61,26 @@ export function PlayerContextProvider({
     setIsLooping(!isLooping);
   }
 
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling);
+  }
+
   function setPlayingState(state: boolean) {
     setIsPlaying(state);
   }
 
   const hasPrevious = currentEpisodeIndex > 0;
-  const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
+  const hasNext = currentEpisodeIndex + 1 < episodeList.length;
 
   function playNext() {
-    if (hasNext) {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(
+        Math.random() * episodeList.length
+      );
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+    } else if (hasNext) {
       setCurrentEpisodeIndex(currentEpisodeIndex + 1);
-    } 
+    }
   }
 
   function playPrevious() {
@@ -88,11 +100,13 @@ export function PlayerContextProvider({
         playPrevious,
         isPlaying,
         isLooping,
+        isShuffling,
         tooglePlay,
+        toggleShuffle,
         setPlayingState,
         hasNext,
         hasPrevious,
-        toogleLoop
+        toogleLoop,
       }}
     >
       {children}
@@ -102,4 +116,4 @@ export function PlayerContextProvider({
 
 export const usePlayer = () => {
   return useContext(PlayerContext);
-}
+};
