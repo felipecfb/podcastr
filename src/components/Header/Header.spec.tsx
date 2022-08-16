134a5d7f24f9a0b38d3d63fc06380ae8e5236498
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-08-16 09:48:13 -0300

    ⚰️ Removing dead code

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,16 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from ".";
 
-jest.mock("next/router", () => {
-  return {
-    useRouter: () => {
-      return {
-        asPath: "/",
-      };
-    },
-  };
-});
-
 describe("Header component", () => {
   it("renders correctly", () => {
     render(<Header />);
